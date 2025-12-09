1efde6b9cabbad52153622fcd8634b901a988339
ajiththomas333 <116882995+ajiththomas333@users.noreply.github.com>
2025-12-09 08:03:14 +0530

    Update Connect.js

--- backend/Connection/Connect.js
+++ backend/Connection/Connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb=async()=>{
     try{
-        const conn=await mongoose.connect("")
+        const conn=await mongoose.connect("mongodb://last-test:Wh4sjaVWuI0tWtOIGvT1dArFpkhwr8nFMd79Mk4DqY9IlZ2C7Y2UcP2cGMS5emK148fyQe9KXIqRACDbiJYloQ==@last-test.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@last-test@")
         console.log("connected")
     }
     catch(err){
